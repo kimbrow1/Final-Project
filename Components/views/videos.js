5aaf6deb7ef96bedf8045c706de798f73f99dfe2
Brian Kimbrow <bkimbrow25@gmail.com>
2020-10-18 04:56:57 -0500

    allnewwork

---
+++ Components/views/videos.js
@@ -0,0 +1,5 @@
+<iframe width="225" height="250" src="https://www.youtube.com/embed/3l3ZqRM3NkI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<iframe width="225" height="250" src="https://www.youtube.com/embed/3l3ZqRM3NkI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<iframe width="225" height="250" src="https://www.youtube.com/embed/3l3ZqRM3NkI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
