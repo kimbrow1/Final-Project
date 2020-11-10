05dcf490ae57605c349b01b7bbb789775f39eb0a
Brian Kimbrow <bkimbrow25@gmail.com>
2020-11-09 20:40:36 -0600

    SavvyCapstone

---
+++ Components/views/Olympics.js
@@ -0,0 +1,21 @@
+export default () => `
+
+<section>
+
+<h2>Special Olympics </h2>
+
+<div class="olympics">
+
+<img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/1036780592-1280x720.jpg?itok=QykjHcAC" alt="Person" style="width:35%">
+
+
+
+<p> The mission of Special Olympics is to provide year-round sports training and athletic competition in a variety of Olympic-type sports for children and adults with intellectual disabilities, giving them continuing opportunities to develop physical fitness, demonstrate courage, experience joy and participate in a sharing of gifts, skills and friendship with their families, other Special Olympics athletes and the community.
+
+The Special Olympics Tennessee has six statewide events annually: Summer Games, Golf, Flag Football, Fall Classic, Winter Games and Basketball. Special Olympics, Inc. holds World Games every other year and USA National Games every four years.
+
+
+</p>
+</div>
+</section>
+`;
